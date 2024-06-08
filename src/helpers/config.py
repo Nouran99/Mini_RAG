7933295602a48fefeb666dfd26261a8bafb77672
nouran99 <nouran7799@gmail.com>
2024-06-08 20:11:16 +0300

    returning a class needs ()

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,18 +1,17 @@
-from pydantic_settings import BaseSettings , SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-class Settings (BaseSettings):
+class Settings(BaseSettings):
 
-    APP_NAME : str
-    APP_VERSION : str
-    OPENAI_API_KEY : str
-    
+    APP_NAME: str
+    APP_VERSION: str
+    OPENAI_API_KEY: str
+
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE: int
-    
+
     class Config:
-        env_file =".env"
+        env_file = ".env"
 
-def get_settings ():
-    return Settings
-
+def get_settings():
+    return Settings()
